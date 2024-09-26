cd7b5f57e4f0a4925bf6ebe9206cb91f4ce3b99c
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-09-26 20:16:08 +0300

    added vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import Intro from "@/components/info";
 import Projects from "@/components/projects";
 import SectionDevider from "@/components/section-devider";
 import Stack from "@/components/stack";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
+      <Analytics />
       <Intro />
       <SectionDevider />
       <About />
