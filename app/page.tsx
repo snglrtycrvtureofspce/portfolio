ddb97085d2438f2f3f6872cc2995896c724ad06c
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-09-26 20:36:34 +0300

    added Vercel Speed Insights

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ import Projects from "@/components/projects";
 import SectionDevider from "@/components/section-devider";
 import Stack from "@/components/stack";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Analytics />
+      <SpeedInsights />
       <Intro />
       <SectionDevider />
       <About />
